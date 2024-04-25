629951cd9bd5aeea4cff8f3f2530854c8e16a54e
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-04-24 22:34:49 -0400

    Create conditional structure for the game

--- script.js
+++ script.js
@@ -23,9 +23,41 @@ function getHumanChoice(){
     let humnChoice=prompt("Select rock,paper, or scissors");
 
     return humanChoice = humnChoice ;
-
     
 }
 
+let humanScore = 0 ;
+let  computerScore= 0 ;
+
+function playRound(humanChoice,compChoice){
+    
+    if  (humanChoice.toLowerCas() !== "rock" || humanChoice.toLowerCas() !== "paper" || humanChoice.toLowerCas() !== "scissors"  ){
+
+        return alert("Please enter a valid option")
+
+    } else if (humanChoice.toLowerCas() === compChoice){
+
+        return console.log (`${humanChoice} and ${compChoice} means a tie!`);
+
+    } else if (humanChoice.toLowerCas() === "rock" && compChoice === "scissors") {
+        
+        console.log (`${humanChoice} win against ${compChoice} !`);
+        return humanScore+=1;
+
+    } else if (humanChoice.toLowerCas() === "paper" && compChoice === "rock"){
+
+        console.log (`${humanChoice} win against ${compChoice} !`);
+        return humanScore+=1;
+
+    }else if (humanChoice.toLowerCas() === "scissors" && compChoice === "paper") {
+
+        console.log (`${humanChoice} win against ${compChoice} !`);
+        return humanScore+=1;
+
+    } else {
+        console.log (`${humanChoice} loses against ${compChoice} !`);
+       return humanScore+=1;;
+    }
+}
 console.log (getComputerChoice());
 console.log (getHumanChoice());
