863b612ae11341461e6d3a750b87d134ea4f9618
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-04-24 21:42:44 -0400

    Create function for player choice

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
+let compChoice=0;
+let humanChoice="";
+
 function getComputerChoice(){
     
      let ramdom= Math.floor(Math.random()*3)+1; // mat floor round the number down, math random generates a number between 0 and 1, multiply by 3, max value now is 2, add one to bring up to 3.
+     
      if (ramdom == 1){
         console.log("rock");
+
      }else if (ramdom == 2) {
         console.log("paper");
+
      } else {
         console.log ("scissors");
      }
      
+     return compChoice = ramdom ;
 }
-console.log(getComputerChoice());
+
+function getHumanChoice(){
+
+    let humnChoice=prompt("Select rock,paper, or scissors");
+
+    return humanChoice = humnChoice ;
+
+    
+}
+
+console.log (getComputerChoice());
+console.log (getHumanChoice());
