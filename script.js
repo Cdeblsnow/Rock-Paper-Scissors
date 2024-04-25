98fcbc650de2e2e03beae03ca790a4a0ced6b2fd
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-04-24 20:16:10 -0400

    Create conditional that associate number to action

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function getComputerChoice(){
     
      let ramdom= Math.floor(Math.random()*3)+1; // mat floor round the number down, math random generates a number between 0 and 1, multiply by 3, max value now is 2, add one to bring up to 3.
-     return ramdom
+     if (ramdom == 1){
+        console.log("rock");
+     }else if (ramdom == 2) {
+        console.log("paper");
+     } else {
+        console.log ("scissors");
+     }
      
 }
 console.log(getComputerChoice());
