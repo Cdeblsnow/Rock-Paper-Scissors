268081e078e3a421f3b9c7b6b65a3175e8b4fd2d
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-05-07 17:53:57 -0400

    Fix the winner message

--- script.js
+++ script.js
@@ -29,41 +29,39 @@ function getPlayerChoice(){
 }           
 
         function playRound(playerChoice,compChoice){
-
-            const winner = document.querySelector("#winner")
-                if ( humanScore == 5) {
-                    winner.textContent = `Congratulations! PLayer wins with a score of ${humanScore}`;
-                    
-            } else if ( computerScore == 5){
-                winner.textContent = `Sorry! the machine wins with a score of ${computerScore}`;
+  
+            if (playerChoice === compChoice){
+                 console.log (`${playerChoice} and ${compChoice} means a tie!`);
+  
+            } else if (playerChoice === "rock" && compChoice === "scissors") {
+                console.log (`${playerChoice} win against ${compChoice} !`);
+                 humanScore+=1;
+        
+            } else if (playerChoice === "paper" && compChoice === "rock"){
+                console.log (`${playerChoice} win against ${compChoice} !`);
+                 humanScore+=1;
+        
+            }else if (playerChoice === "scissors" && compChoice === "paper") {
+                console.log (`${playerChoice} win against ${compChoice} !`);
+                 humanScore+=1;
+        
+            } else {
+                console.log (`${playerChoice} loses against ${compChoice} !`);
+                computerScore+=1;;
             }
 
+            const winner = document.querySelector("#winner")
             const playerScore = document.querySelector("#playerScore")
             const compScore = document.querySelector("#compScore")
                 playerScore.textContent = "Player score: " + humanScore;
                 compScore.textContent= "Machine score: " + computerScore;
-                
-            if (playerChoice === compChoice){
-                return console.log (`${playerChoice} and ${compChoice} means a tie!`);
-  
-            } else if (playerChoice === "rock" && compChoice === "scissors") {
-                console.log (`${playerChoice} win against ${compChoice} !`);
-                return humanScore+=1;
-        
-            } else if (playerChoice === "paper" && compChoice === "rock"){
-                console.log (`${playerChoice} win against ${compChoice} !`);
-                return humanScore+=1;
-        
-            }else if (playerChoice === "scissors" && compChoice === "paper") {
-                console.log (`${playerChoice} win against ${compChoice} !`);
-                return humanScore+=1;
-        
-            } else {
-                console.log (`${playerChoice} loses against ${compChoice} !`);
-               return computerScore+=1;;
-            }
-
-            
+                if ( humanScore == 5) {
+                    winner.textContent = `Congratulations! PLayer wins with a score of ${humanScore}`;
+                    return;
+                } else if ( computerScore == 5){
+                    winner.textContent = `Sorry! the machine wins with a score of ${computerScore}`;
+                    return;
+                }
         }
             
 getPlayerChoice();
